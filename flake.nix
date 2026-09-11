85961d4c708a0f891489b9fd96c0d26d7b51b270
Artem Kuznetsov <artyom.20century@gmail.com>
2026-09-12 01:24:56 +0200

    chore(nix): add Flake and package derivation for macicon

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Generate, customize, and apply Apple continuous-curvature squircle app icons on macOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./nix/package.nix {};
+        macicon = pkgs.callPackage ./nix/package.nix {};
+      });
+
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/macicon";
+        };
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+          ];
+        };
+      });
+    };
+}
